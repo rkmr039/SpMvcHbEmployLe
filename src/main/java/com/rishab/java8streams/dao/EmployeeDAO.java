95a0cfb4bb41c0140db709815c770503b3b63f60
rkmr039 <rkmr039@gmail.com>
2019-09-17 12:05:31 +0530

    Employee DAO Done

--- src/main/java/com/rishab/java8streams/dao/EmployeeDAO.java
+++ src/main/java/com/rishab/java8streams/dao/EmployeeDAO.java
@@ -6,11 +6,18 @@ import com.rishab.java8streams.model.Employee;
 
 public interface EmployeeDAO {
 
+	// SELECT
 	public List<Employee> getAllEmployee();
-	public List<Employee> getAllEmployeeByBand();
-	public Employee getEmployeeById();
-	public void addEmployee();
-	public Employee updateEmployee();
-	public void deleteEmployee();
+	public List<Employee> getAllEmployeeByBand(char band);
+	public Employee getEmployeeById(int empNo);
+	
+	// INSERT
+	public void addEmployee(Employee employee);
+	
+	// UPDATE
+	public Employee updateEmployee(Employee employee);
+	
+	// DELETE
+	public void deleteEmployee(int empNo);
 	
 }
